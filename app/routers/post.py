331799cda1fc675127061b6a6912c2a8518c18a8
zaidxB <bolajiogundele7@gmail.com>
2026-07-24 11:49:34 +0100

    post.py

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,14 +12,11 @@ router = APIRouter(
     tags=["Posts"]
     )
 
-#getting all post
-# @router.get("/", response_model= List[schemas.Post])
+
 @router.get("/", response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
               limit: int =10, skip: int =0, search: Optional[str] = ""):
-    # cursor.execute("""SELECT * FROM posts """)
-    # posts = cursor.fetchall()
-    #posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+   
 
     posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote,
     models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(
@@ -30,11 +27,7 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
 #creating a post
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model= schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
-    #                (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-
-    # conn.commit()
+   
 
     new_post = models.Post(owner_id=current_user.id, **post.model_dump())
     db.add(new_post)
@@ -47,16 +40,11 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
 @router.get("/{id}", response_model= schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
-    # post = cursor.fetchone()
-
-    #post = db.query(models.Post).filter(models.Post.id == id).first()
+  
     
     post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote,models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
 
-    #if post.owner_id != current_user.id:
-        #raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
-  
+ 
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -68,12 +56,7 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
 #Deleting post
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    #deleting post
-    #find the index in the array that has required ID
-    #my_post.pop(index)
-    # cursor.execute("""DELETE FROM posts WHERE id = %s returning * """, (str(id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
+    
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
@@ -92,10 +75,7 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
 #Update post
 @router.put("/{id}",response_model= schemas.Post)
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    # cursor.execute("""UPDATE posts SET title= %s, content= %s, published= %s WHERE id = %s RETURNING * """,
-    #                (post.title, post.content, post.published, str(id)))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
+  
     post_query = db.query(models.Post).filter(models.Post.id == id)
 
     post = post_query.first()
